5b34d6caf202348bf0c8c6db144c4617380f0bda
Christoph Wende <codeporn@users.noreply.github.com>
2024-01-23 12:19:36 +0000

    Fixed level 1

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -12,22 +12,36 @@ Follow the instructions below to get started:
 '''
 
 from collections import namedtuple
+from decimal import Decimal
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+MAX_ORDER_VALUE = 100000
+MIN_ORDER_VALUE = 0
+MIN_QUANTITY = 0
+MAX_QUANTITY = 100
+
 def validorder(order: Order):
-    net = 0
+
+    payments = Decimal(0)
+    orderValue = Decimal(0)
 
     for item in order.items:
+
         if item.type == 'payment':
-            net += item.amount
+            if -MAX_ORDER_VALUE <= item.amount <= MAX_ORDER_VALUE:
+                payments += Decimal(str(item.amount))
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            if type(item.quantity) is int and MIN_QUANTITY < item.quantity <= MAX_QUANTITY and MIN_ORDER_VALUE < item.amount <= MAX_ORDER_VALUE:
+                orderValue += Decimal(str(item.amount)) * item.quantity
         else:
             return "Invalid item type: %s" % item.type
 
-    if net != 0:
-        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
+    if abs(payments) > MAX_ORDER_VALUE or orderValue > MAX_ORDER_VALUE:
+        return "Total amount payable for an order exceeded"
+
+    if payments != orderValue:
+        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, payments - orderValue)
     else:
         return "Order ID: %s - Full payment received!" % order.id
